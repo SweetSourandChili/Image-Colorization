b89199cb4b6b5f6b6da71eec492d60475aa7d732
Arda Meriç <ardam416@gmail.com>
2023-01-06 15:01:21 +0300

    hypermarameter tunning

--- main.py
+++ main.py
@@ -9,21 +9,30 @@ import torchvision.transforms as transforms
 import hw3utils
 
 batch_size = 16
-max_num_epoch = 100
-hps = {'lr': 0.001}
-num_kernels = 8
+max_num_epoch = 3
+num_kernels = (2, 4, 8)
+learning_rates = (0.0001, 0.001, 0.1)
+conv_layers = (1, 2, 4)
 
 # ---- options ----
 DEVICE_ID = 'cpu'  # set to 'cpu' for cpu, 'cuda' / 'cuda:0' or similar for gpu.
 LOG_DIR = 'checkpoints'
 VISUALIZE = False  # set True to visualize input, prediction and the output from the last batch
 LOAD_CHKPT = False
+SAVE_MODEL = False
 
 torch.multiprocessing.set_start_method('spawn', force=True)
 if torch.backends.mps.is_available():
     DEVICE_ID = "mps"
 
+
 # ---- utility functions -----
+
+
+def write_output(filename, string_to_write):
+    f = open(os.path.join("outputs", filename), "w")
+    f.write(string_to_write)
+    f.close()
 
 
 def get_loaders(batch_size, device):
@@ -38,77 +47,108 @@ def get_loaders(batch_size, device):
 
 # ---- ConvNet -----
 class Net(nn.Module):
-    def __init__(self):
+    def __init__(self, conv_layers, num_kernel):
         super(Net, self).__init__()
-        self.conv1 = nn.Conv2d(1, num_kernels, kernel_size=3, padding=1)
-        self.conv2 = nn.Conv2d(num_kernels, num_kernels, kernel_size=3, padding=1)
-        self.conv3 = nn.Conv2d(num_kernels, num_kernels, kernel_size=3, padding=1)
-        self.conv4 = nn.Conv2d(num_kernels, 3, kernel_size=3, padding=1)
+        if conv_layers <= 1:
+            num_kernel = 1
+        self.conv_layers = conv_layers
+        self.conv1 = nn.Conv2d(1, num_kernel, kernel_size=3, padding=1)
+        self.conv2 = nn.Conv2d(num_kernel, num_kernel, kernel_size=3, padding=1)
+        self.conv3 = nn.Conv2d(num_kernel, num_kernel, kernel_size=3, padding=1)
+        self.convs = (self.conv1, self.conv2, self.conv3)
+        self.conv4 = nn.Conv2d(num_kernel, 3, kernel_size=3, padding=1)
         self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
 
-    def forward(self, grayscale_image):
+    def forward(self, x):
         # apply your network's layers in the following lines:
-        x = F.relu(self.conv1(grayscale_image))
-        x = F.relu(self.conv2(x))
-        x = F.relu(self.conv3(x))
+        for i in range(self.conv_layers - 1):
+            x = F.relu(self.convs[i](x))
         x = self.conv4(x)
         x = x.view(-1, 3, 80, 80)
         return x
 
 
-# ---- training code -----
-device = torch.device(DEVICE_ID)
-print('device: ' + str(device))
-net = Net().to(device=device)
-criterion = nn.MSELoss()
-optimizer = optim.SGD(net.parameters(), lr=hps['lr'])
-train_loader, val_loader = get_loaders(batch_size, device)
-
 if LOAD_CHKPT:
     print('loading the model from the checkpoint')
     model.load_state_dict(os.path.join(LOG_DIR, 'checkpoint.pt'))
 
-last_val_loss = float("inf")
-print('training begins')
-for epoch in range(max_num_epoch):
-    running_loss = 0.0  # training loss of the network
-    for iteri, data in enumerate(train_loader, 0):
-        inputs, targets = data  # inputs: low-resolution images, targets: high-resolution images.
 
-        net.train()
-        optimizer.zero_grad()  # zero the parameter gradients
+def train_model(conv_layer, num_kernel, lr):
+    string_to_write = ""
+    # ---- training code -----
+    device = torch.device(DEVICE_ID)
+    print('device: ' + str(device))
+    net = Net(conv_layer, num_kernel).to(device=device)
+    criterion = nn.MSELoss()
+    optimizer = optim.SGD(net.parameters(), lr=lr)
+    train_loader, val_loader = get_loaders(batch_size, device)
 
-        # do forward, backward, SGD step
-        preds = net(inputs)
-        loss = criterion(preds, targets)
-        loss.backward()
-        optimizer.step()
-        running_loss += loss.item()
+    last_val_loss = float("inf")
+    print('training begins')
+    for epoch in range(max_num_epoch):
+        running_loss = 0.0  # training loss of the network
+        for iteri, data in enumerate(train_loader, 0):
+            inputs, targets = data  # inputs: low-resolution images, targets: high-resolution images.
 
-        if (iteri == 0) and VISUALIZE:
-            hw3utils.visualize_batch(inputs, preds, targets)
+            net.train()
+            optimizer.zero_grad()  # zero the parameter gradients
 
-    print('Saving the model, end of epoch %d' % (epoch + 1))
-    if not os.path.exists(LOG_DIR):
-        os.makedirs(LOG_DIR)
-    torch.save(net.state_dict(), os.path.join(LOG_DIR, 'checkpoint.pt'))
-    hw3utils.visualize_batch(inputs, preds, targets, os.path.join(LOG_DIR, 'example.png'))
+            # do forward, backward, SGD step
+            preds = net(inputs)
+            loss = criterion(preds, targets)
+            loss.backward()
+            optimizer.step()
+            running_loss += loss.item()
 
-    # validation loss
-    with torch.no_grad():
+            if (iteri == 0) and VISUALIZE:
+                hw3utils.visualize_batch(inputs, preds, targets)
+
+        if SAVE_MODEL:
+            print('Saving the model, end of epoch %d' % (epoch + 1))
+            if not os.path.exists(LOG_DIR):
+                os.makedirs(LOG_DIR)
+            torch.save(net.state_dict(), os.path.join(LOG_DIR, 'checkpoint.pt'))
+        hw3utils.visualize_batch(inputs, preds, targets, os.path.join(LOG_DIR, 'example.png'))
+
+        # validation loss
+        with torch.no_grad():
+            val_loss = 0.0
+            for ind, (val_inputs, val_targets) in enumerate(val_loader):
+                val_preds = net.forward(val_inputs)
+                val_loss_iter = criterion(val_preds, val_targets)
+                val_loss += val_loss_iter.item()
+        string_to_write += 'Epoch = {} | Train Loss = {:.2f}\tVal Loss = {:.2f}\n'.format(epoch + 1, running_loss, val_loss)
+        # stop epochs if not improving
+        if (last_val_loss - val_loss) < 0.01:
+            print("The number of epochs of the model is: %d" % (epoch + 1))
+            break
+        last_val_loss = val_loss
         val_loss = 0.0
-        for ind, (val_inputs, val_targets) in enumerate(val_loader):
-            val_preds = net.forward(val_inputs)
-            val_loss_iter = criterion(val_preds, val_targets)
-            val_loss += val_loss_iter.item()
-    print('Epoch = {} | Train Loss = {:.2f}\tVal Loss = {:.2f}'.format(epoch+1, running_loss, val_loss))
-    # stop epochs if not improving
-    print(last_val_loss-val_loss)
-    if (last_val_loss - val_loss) < 0.1:
-        print("The number of epochs of the model is: %d" % (epoch + 1))
-        break
-    last_val_loss = val_loss
-    val_loss = 0.0
-    running_loss = 0.0
+        running_loss = 0.0
+    write_output("Conv_layer:{}\tNum_kernel:{}\tlr:{}".format(conv_layer, num_kernel, lr), string_to_write)
+    print('Finished Training')
+    return net, epoch, last_val_loss
 
-print('Finished Training')
+
+least_loss = float("inf")
+best_conf = {"conv_layer": None, "num_kernel": None, "learning_rate": None, "best": ""}
+for conv_layer in conv_layers:
+    for num_kernel in num_kernels:
+        for learning_rate in learning_rates:
+            print("Configuration:")
+            configuration = "Convolution Layers: {}\tNumber of Kernels: {}\t Learning Rate: {}".format(conv_layer,
+                                                                                                       num_kernel,
+                                                                                                       learning_rate)
+            print(configuration)
+            model, epoch, last_val = train_model(conv_layer, num_kernel, learning_rate)
+            print("Epochs stopped at: {}\t Validation Loss: {}".format(epoch, last_val))
+            if least_loss > last_val:
+                least_loss = last_val
+                best_conf["conv_layer"] = conv_layer
+                best_conf["num_kernel"] = num_kernel
+                best_conf["learning_rate"] = learning_rate
+                best_conf["best"] = configuration
+
+print("The best configuration:\n" + best_conf["best"])
+print("Train model with the best configuration...")
+model, _, _ = train_model(best_conf["conv_layer"], best_conf["num_kernel"], best_conf["learning_rate"])
