f1ec7ab6e520cf9b8cca746cdd6f3c7327bb72c3
Arda Meriç <ardam416@gmail.com>
2023-01-14 21:18:08 +0300

    last commit

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ import torch.nn.functional as F
 import torch.optim as optim
 import torchvision.transforms as transforms
 import hw3utils
+import cv2
 
 batch_size = 16
 max_num_epoch = 100
@@ -22,11 +23,21 @@ LOAD_CHKPT = False
 SAVE_MODEL = False
 
 torch.multiprocessing.set_start_method('spawn', force=True)
+if torch.cuda.is_available():
+    DEVICE_ID = "cuda"
 if torch.backends.mps.is_available():
     DEVICE_ID = "mps"
 
-
+device = torch.device(DEVICE_ID)
 # ---- utility functions -----
+
+
+def plot_graph(x, y, name):
+    plt.title(name)
+    plt.xlabel('Epochs')
+    plt.ylabel('Values')
+    plt.plot(x, y)
+    plt.show()
 
 
 def write_output(filename, string_to_write):
@@ -41,8 +52,23 @@ def get_loaders(batch_size, device):
     train_loader = torch.utils.data.DataLoader(train_set, batch_size=batch_size, shuffle=True, num_workers=0)
     val_set = hw3utils.HW3ImageFolder(root=os.path.join(data_root, 'val'), device=device)
     val_loader = torch.utils.data.DataLoader(val_set, batch_size=batch_size, shuffle=False, num_workers=0)
-    # Note: you may later add test_loader to here.
     return train_loader, val_loader
+
+
+def load_images_from_folder(folder, num):
+    count = 0
+    images = []
+    filenames = []
+    for filename in os.listdir(folder):
+        full_name = os.path.join(folder,filename)
+        img = cv2.imread(full_name, 0)
+        if img is not None:
+            images.append(img)
+            filenames.append(full_name)
+            count += 1
+        if count >= num:
+            break
+    return images, filenames
 
 
 # ---- ConvNet -----
@@ -63,10 +89,9 @@ class Net(nn.Module):
         # apply your network's layers in the following lines:
         for i in range(self.conv_layers - 1):
             x = F.relu(self.convs[i](x))
-        x = torch.tanh(self.conv4(x))
+        x = self.conv4(x)
         x = x.view(-1, 3, 80, 80)
         return x
-
 
 if LOAD_CHKPT:
     print('loading the model from the checkpoint')
@@ -76,27 +101,28 @@ if LOAD_CHKPT:
 def train_model(conv_layer, num_kernel, lr):
     string_to_write = ""
     # ---- training code -----
-    device = torch.device(DEVICE_ID)
     print('device: ' + str(device))
     net = Net(conv_layer, num_kernel).to(device=device)
-    criterion = nn.MSELoss()
-    margin_loss = nn.MarginRankingLoss()
+    # criterion = nn.MSELoss()
+    criterion = nn.MarginRankingLoss(margin=12)
     optimizer = optim.SGD(net.parameters(), lr=lr)
     train_loader, val_loader = get_loaders(batch_size, device)
 
+    net.train()
     last_val_loss = float("inf")
     print('training begins')
+    loss_arr = []
+    m_loss_arr = []
     for epoch in range(max_num_epoch):
         running_loss = 0.0  # training loss of the network
         for iteri, data in enumerate(train_loader, 0):
             inputs, targets = data  # inputs: low-resolution images, targets: high-resolution images.
 
-            net.train()
             optimizer.zero_grad()  # zero the parameter gradients
 
             # do forward, backward, SGD step
             preds = net(inputs)
-            loss = criterion(preds, targets)
+            loss = criterion(inputs * 0x00010101, preds, targets)
             loss.backward()
             optimizer.step()
             running_loss += loss.item()
@@ -104,6 +130,7 @@ def train_model(conv_layer, num_kernel, lr):
             if (iteri == 0) and VISUALIZE:
                 hw3utils.visualize_batch(inputs, preds, targets)
 
+        loss_arr.append(running_loss)
         print(epoch+1, end="-")
         if SAVE_MODEL:
             print('Saving the model, end of epoch %d' % (epoch + 1))
@@ -111,7 +138,7 @@ def train_model(conv_layer, num_kernel, lr):
                 os.makedirs(LOG_DIR)
             torch.save(net.state_dict(), os.path.join(LOG_DIR, 'checkpoint.pt'))
 
-        hw3utils.visualize_batch(inputs, preds, targets, os.path.join(LOG_DIR, 'example.png'))
+        #hw3utils.visualize_batch(inputs, preds, targets, os.path.join(LOG_DIR, 'example.png'))
 
         # validation loss
         with torch.no_grad():
@@ -119,7 +146,7 @@ def train_model(conv_layer, num_kernel, lr):
             val_loss = 0.0
             for ind, (val_inputs, val_targets) in enumerate(val_loader):
                 val_preds = net.forward(val_inputs)
-                val_loss_iter = margin_loss(12, val_preds, val_targets)
+                val_loss_iter = criterion(val_inputs* 0x00010101, val_preds, val_targets)      # convert grayscale to rgb representation
                 val_loss += val_loss_iter.item()
         string_to_write += 'Epoch = {} | Train Loss = {:.2f}\tVal Loss = {:.2f}\n'.format(epoch + 1, running_loss, val_loss)
         # stop epochs if not improving
@@ -129,7 +156,7 @@ def train_model(conv_layer, num_kernel, lr):
         last_val_loss = val_loss
 
     write_output("Conv_layer:{}\tNum_kernel:{}\tlr:{}".format(conv_layer, num_kernel, lr), string_to_write)
-    print('Finished Training')
+    print('\nFinished Training')
     return net, epoch, last_val_loss
 
 
@@ -153,6 +180,24 @@ for conv_layer in conv_layers:
                 best_conf["learning_rate"] = learning_rate
                 best_conf["best"] = configuration
 """
+
+
 print("The best configuration:\n" + best_conf["best"])
 print("Train model with the best configuration...")
 model, _, _ = train_model(best_conf["conv_layer"], best_conf["num_kernel"], best_conf["learning_rate"])
+
+test_images, test_names = load_images_from_folder("ceng483-f22-hw3-dataset/images_grayscale",100)
+transform = transforms.Compose([transforms.ToTensor()])
+
+
+test_images = [transform(img).to(device) for img in test_images]
+predictions = [model.forward(test_images[i]).cpu().detach().numpy() for i in range(100)]
+predictions = np.array(predictions).reshape((100, 80, 80, 3))
+
+# write npy and names.txt
+np.save("estimations", predictions)
+with open("img_names.txt", 'w') as f:
+    for line in test_names:
+        f.write(f"{line}\n")
+
+
